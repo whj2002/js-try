41350e9542200860954904b9ec42d53f8cd5885f
Wu <1575440859@qq.com>
2020-10-16 11:59:14 +0800

    初始

---
+++ one.js
@@ -0,0 +1,16 @@
+function b1() {document.getElementById("a1").innerHTML = "嘿";}
+function b2() {window.alert(5*9+65);}
+function b3() {document.write(404);}
+function b4() 
+    {
+        var a,b,c;
+        a="a";
+        b="b";
+        c=a+b;
+        document.getElementById("a1").innerHTML=c;
+    }
+function b5() 
+    {
+        var a="选课人数未发生变化";
+        window.alert(a);
+    }
